06db35561e5c8f2c43e83cb39eaa3217e3bca612
camicaicedof <124204338+camicaicedof@users.noreply.github.com>
2023-11-14 10:35:34 -0500

    Delete proyecto2 directory

--- proyecto2/main2.py
+++
@@ -1,8 +0,0 @@
-from controller import AeropuertoController
-from aeropuerto import Aeropuerto
-def main():
-    aeropuerto= Aeropuerto()
-    controller=AeropuertoController(aeropuerto)
-    controller.ejecutar()
-
-main()
